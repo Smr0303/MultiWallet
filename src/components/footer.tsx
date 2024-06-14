54767788174c82dd8d4299ac3703302fab85b047
Smr0303 <samarmohite7@gmail.com>
2024-06-15 02:37:43 +0530

    Frontend completed and pushed

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="w-full px-6 border-t border-t-gray-700 py-2 text-center">
+      Build Your own MultiSigWallet with Account Abstraction
+      !
+    </div>
+  );
+}
